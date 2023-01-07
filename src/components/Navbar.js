2427d678ef88f14715fce054a135f1b1a25b0012
Andy Leon <andyleon000@gmail.com>
2023-01-07 15:58:10 -0500

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react"
+import logo from "../images/logo.png"
 
 function Navbar(){
     return (
         <div>
-            <p>test</p>
+            <img src={logo}></img>
         </div>
     )
 }
